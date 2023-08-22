67d91efa634abde9be9db660cf1a21e3b606c08d
Sandalu Pabasara <sandalu.tharuprocessing@gmail.com>
2023-08-22 16:06:24 +0530

    Tflite implemantation

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ import io
 
 app = Flask(__name__)
 
-# Load the model
-model = load_model('2_best_leaf_model.h5')
+# Load the TFLite model and allocate tensors
+interpreter = tf.lite.Interpreter(model_path='2_best_leaf_model.tflite')
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 def adjust_brightness_and_saturation(image):
     image = tf.image.adjust_brightness(image, delta=0.2)
@@ -29,15 +33,23 @@ def predict():
     image_arr = np.asarray(image) / 255.0  # Convert to numpy array and normalize
     image_arr = adjust_brightness_and_saturation(image_arr)
     image_arr = np.expand_dims(image_arr, axis=0)  # Expand dimensions for batch input
+    image_arr = image_arr.astype(np.float32)  # Cast to float32
     
-    class_lables = ['disease1','disease2','disease3','healthy']
+    class_labels = ['disease1','disease2','disease3','healthy']
 
-    # Predict
-    predictions = model.predict(image_arr)
+    # Set the tensor (the input tensor is usually index 0 for TFLite models)
+    interpreter.set_tensor(input_details[0]['index'], image_arr)
+    
+    # Invoke the interpreter to perform inference
+    interpreter.invoke()
+    
+    # Retrieve the output tensor (the output tensor is usually index 0 for TFLite models)
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+    
     predicted_class = np.argmax(predictions[0])
     confidence = predictions[0][predicted_class]
     
-    return jsonify({'class': str(class_lables[predicted_class]), 'confidence': str(confidence)})
+    return jsonify({'class': str(class_labels[predicted_class]), 'confidence': str(confidence)})
 
 if __name__ == '__main__':
     app.run(debug=True)
